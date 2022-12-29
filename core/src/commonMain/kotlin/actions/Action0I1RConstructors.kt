33c7594589a3f94ec3491dfb7b5e2d07723a5443
andylamax <andylamax@programmer.net>
2022-12-29 19:00:02 +0300

    Fixed the nasty selector bug

--- core/src/commonMain/kotlin/actions/Action0I1RConstructors.kt
+++ core/src/commonMain/kotlin/actions/Action0I1RConstructors.kt
@@ -3,6 +3,13 @@ package actions
 import actions.internal.MutableAction0I1RImpl
 import koncurrent.Later
 
+inline fun action0I0R(
+    name: String,
+    noinline handler: () -> Unit
+): Action0I1R<Unit> = MutableAction0I1RImpl(name) {
+    Later(Unit).then { handler() }
+}
+
 inline fun <R> action0I1RLater(
     name: String,
     noinline handler: () -> Later<R>
@@ -24,5 +31,12 @@ inline fun <R> mutableAction0I1R(
     name: String,
     noinline handler: () -> R
 ): MutableAction0I1R<R> = MutableAction0I1RImpl(name) {
+    Later(Unit).then { handler() }
+}
+
+inline fun mutableAction0I0R(
+    name: String,
+    noinline handler: () -> Unit
+): MutableAction0I1R<Unit> = MutableAction0I1RImpl(name) {
     Later(Unit).then { handler() }
 }
